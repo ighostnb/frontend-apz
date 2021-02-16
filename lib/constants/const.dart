3bfe83c6aa8bb7099b8991f90e4cb4ba88d62481
ighostnb <ighostnb@gmail.com>
2021-02-17 01:44:36 +0300

    auth repository

---
+++ lib/constants/const.dart
@@ -0,0 +1,5 @@
+class Const {
+  static const String registerUrl = '';
+  static const String loginUrl = '';
+  static const String deleteUserUrl = '';
+}
--- lib/repositories/auth_repository.dart
+++ lib/repositories/auth_repository.dart
@@ -1,5 +1,80 @@
+import 'dart:convert';
+
+import 'package:frontend_apz/constants/const.dart';
 import 'package:http/http.dart' as http;
 
-abstract class AuthRepository {}
+abstract class AuthRepository {
+  Future<void> createUser({String username, String password});
+  Future<void> login({String username, String password});
+  Future<void> deleteUser({String id, String token});
+}
 
-class AuthRepositoryImpl extends AuthRepository {}
+class AuthRepositoryImpl extends AuthRepository {
+  @override
+  Future<void> createUser({String username, String password}) async {
+    try {
+      final _headers = {
+        'Content-type': 'application/json',
+      };
+
+      final _body = jsonEncode({
+        'username': username,
+        'password': password,
+      });
+
+      final _response = await http.post(
+        Const.registerUrl,
+        body: _body,
+        headers: _headers,
+      );
+
+      if (_response.statusCode == 200) {}
+    } catch (__) {
+      print(__.toString());
+    }
+  }
+
+  @override
+  Future<void> deleteUser({String id, String token}) async {
+    try {
+      final _headers = {
+        'Content-type': 'application/json',
+        'id': id,
+        'token': token,
+      };
+
+      final _response = await http.delete(
+        Const.deleteUserUrl,
+        headers: _headers,
+      );
+
+      if (_response.statusCode == 200) {}
+    } catch (__) {
+      print(__.toString());
+    }
+  }
+
+  @override
+  Future<void> login({String username, String password}) async {
+    try {
+      final _headers = {
+        'Content-type': 'application/json',
+      };
+
+      final _body = jsonEncode({
+        'username': username,
+        'password': password,
+      });
+
+      final _response = await http.post(
+        Const.loginUrl,
+        body: _body,
+        headers: _headers,
+      );
+
+      if (_response.statusCode == 200) {}
+    } catch (__) {
+      print(__.toString());
+    }
+  }
+}
